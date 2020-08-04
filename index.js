d361021504cd720b4eb0a8cb34df2038aa3951c5
Ricardo Baniski <45471710+RicardoBaniski@users.noreply.github.com>
2020-08-04 20:02:36 -0300

    65. N04 - Detectando URL diferente

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const http = require('http');
 let server = http.createServer((req, res) => {
     console.log('URL', req.url);
     console.log('METHOD', req.method);
-    res.end('OK');
+
+    switch (req.url) {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Olá</h1');
+            break;
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                users: [{
+                    name: 'Rbaniski',
+                    email: 'rbaniski@gmail.com',
+                    id: 1
+                }]
+            }));
+            break;
+    }
 });
 
 server.listen(3000, '127.0.0.1', () => {
