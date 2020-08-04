1a4f1bc0a8ba909bf9ebe6719f2709386bba26df
Ricardo Baniski <45471710+RicardoBaniski@users.noreply.github.com>
2020-08-04 20:32:44 -0300

    67. N06 - Nodemon e Criando o Servidor com Express

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-const http = require('http');
+const express = require('express');
+let app = express();
 
-let server = http.createServer((req, res) => {
-    console.log('URL', req.url);
-    console.log('METHOD', req.method);
-
-    switch (req.url) {
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Olá</h1');
-            break;
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'Rbaniski',
-                    email: 'rbaniski@gmail.com',
-                    id: 1
-                }]
-            }));
-            break;
-    }
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá</h1');
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.get('/users', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [{
+            name: 'Rbaniski',
+            email: 'rbaniski@gmail.com',
+            id: 1
+        }]
+    });
+});
+
+app.listen(3000, '127.0.0.1', () => {
     console.log('servidor rodando!');
 });
