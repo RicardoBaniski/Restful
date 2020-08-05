5221657bef3ebc0f219b22f85a4bcc820ee746fb
Ricardo Baniski <45471710+RicardoBaniski@users.noreply.github.com>
2020-08-04 21:49:35 -0300

    74. N13 - Obtendo dados de um usuário

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ let db = new NeDB({
 
 module.exports = (app) => {
     let route = app.route('/users');
+
     route.get((req, res) => {
         db.find({}).sort({ name: 1 }).exec((err, users) => {
             if (err) {
@@ -28,4 +29,17 @@ module.exports = (app) => {
             }
         })
     });
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+        db.findOne({ _id: req.params.id }).exec((err,user)=>{
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
+        });
+    });
+
 };
